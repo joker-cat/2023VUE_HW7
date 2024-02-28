77782f7077654508658fee419926c489f34ce8ae
joker <jocker.ss.cat@gmail.com>
2024-02-29 01:38:58 +0800

    優惠券問題，沒id & 完成:新增/編輯優惠券

--- src/components/backendPage/AdminCoupons.vue
+++ src/components/backendPage/AdminCoupons.vue
@@ -1,33 +1,31 @@
 <template>
   <div class="row my-4 position-relative">
-    <div class="col-sm-4 col-6 mb-4" v-for="icoupon in coupons" :key="icoupon.id">
+    <div class="col-sm-4 col-6 mb-4" v-for="icoupon in coupons" :key="icoupon.id" @click="editCoupon($event, icoupon)">
       <div class="card">
         <img src="../../assets/image/coupon.png" class="card-img-top" alt="img error">
         <div class="card-body">
-          <p class="card-text">名稱 {{ icoupon.title }}</p>
-          <p class="card-text">折扣 {{ icoupon.percent }}</p>
-          <p class="card-text">狀態 {{ icoupon.is_enabled?'啟用':'不啟用' }}</p>
+          <div class="row fs-6">
+            <div class="col-4">
+              <p class="card-text" style="font-weight: 500;">名稱</p>
+            </div>
+            <div class="col-8 text-danger mb-1">{{ icoupon.title }}</div>
+            <div class="col-4">
+              <p class="card-text" style="font-weight: 500;">折扣</p>
+            </div>
+            <div class="col-8 text-danger mb-1">{{ icoupon.percent }}</div>
+            <div class="col-4">
+              <p class="card-text" style="font-weight: 500;">是否啟用</p>
+            </div>
+            <div class="col-8 text-danger">
+              <div class="form-check form-switch ps-0">
+                <input class="form-check-input form-control m-0 mt-1" type="checkbox" id="flexSwitchCheckDefault"
+                  v-model="icoupon.is_enabled" true-value="1" false-value="0" @change="handleCheckboxChange(icoupon)">
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <!-- <div class="col-sm-4 col-6">
-      <div class="card">
-        <img src="../../assets/image/coupon.png" class="card-img-top" alt="img error">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-4 col-6">
-      <div class="card">
-        <img src="../../assets/image/coupon.png" class="card-img-top" alt="img error">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
-      </div>
-    </div> -->
     <button type="button" class="btn btn-dark position-absolute" style="width: 120px;top: -50px;right: 12px;"
       @click="openModal">
       新增優惠券
@@ -41,7 +39,8 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title fw-bolder" id="exampleModalLabel">新增優惠券</h5>
+          <h5 class="modal-title fw-bolder" id="exampleModalLabel">{{ couponApi === 'post' ? '新增' : '編輯' }}優惠券</h5>
+          <span class="text-danger">(14天期限)</span>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -52,6 +51,7 @@
               </div>
               <div class="col-md-8">
                 <div class="card-body p-0">
+                  {{ couponForm.data }}
                   <form>
                     <div class="mb-3">
                       <label for="cardtitle">優惠券名稱</label>
@@ -62,20 +62,16 @@
                       <input type="text" class="form-control" id="exampleInputEmail1"
                         v-model.number="couponForm.data.percent">
                     </div>
-                    <div class="mb-3 form-check">
-                      <input type="checkbox" class="form-check-input" id="exampleCheck1"
-                        v-model="couponForm.data.is_enabled" true-value="1" false-value="0">
-                      <label class="form-check-label" for="exampleCheck1">是否啟用 {{ typeof couponForm.data.is_enabled
-                      }}</label>
+                    <div class="mb-3 form-check ps-0">
+                      <div class="form-check form-switch ps-0">
+                        <div class="d-flex">
+                          <label class="form-check-label me-2" for="flexSwitchCheckDefault">是否啟用</label>
+                          <input class="form-check-input form-control m-0 mt-1" type="checkbox"
+                            id="flexSwitchCheckDefault" v-model="couponForm.data.is_enabled" true-value="1"
+                            false-value="0">
+                        </div>
+                      </div>
                     </div>
-                    <!-- <div class="mb-3">
-                      <label for="exampleInputPassword1">due_date</label>
-                      <input type="text" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <div class="mb-3">
-                      <label for="exampleInputPassword1">code</label>
-                      <input type="text" class="form-control" id="exampleInputPassword1">
-                    </div> -->
                   </form>
                 </div>
               </div>
@@ -83,8 +79,10 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn fs-6 p-2 px-3 border border-dark" data-bs-dismiss="modal">取消</button>
-          <button type="button" class="btn fs-6 p-2 px-3 bg-dark text-light" @click="createCoupon">新增</button>
+          <button type="button" class="btn fs-6 p-2 px-3 btn-danger me-auto"  @click="deleteCoupon(couponForm.data.id)">刪除</button>
+            <button type="button" class="btn fs-6 p-2 px-3 border border-dark" data-bs-dismiss="modal">取消</button>
+          <button type="button" class="btn fs-6 p-2 px-3 bg-dark text-light" @click="createCoupon">{{ couponApi === 'post'
+            ? '新增' : '編輯' }}</button>
         </div>
       </div>
     </div>
@@ -97,13 +95,14 @@ export default {
   data() {
     return {
       modal: null,
+      couponApi: 'post',
       coupons: [],
       couponForm: {
         data: {
           title: "",
           is_enabled: 0,
           percent: null,
-          due_date: new Date().getTime(),
+          due_date: 0,
           code: "testCode"
         }
       }
@@ -114,29 +113,80 @@ export default {
       this.$axios
         .get('/admin/coupons')
         .then((res) => {
+          console.log(res);
           this.coupons = res.data.coupons;
         })
         .catch(() => {
         })
     },
+    getTimeStamp() {
+      let currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + 14); // 14天後過期
+      let threeDaysLaterTimestamp = currentDate.getTime();
+      return threeDaysLaterTimestamp
+    },
+    deleteCoupon(id) {
+      console.log(id);
+      this.$axios
+        .delete(`/admin/coupon/${id}`)
+        .then((res) => {
+          if (res.data.success) alert('已刪除優惠券');
+          this.closeModal();
+          this.getCoupons();
+        })
+        .catch((err) => console.log(err))
+    },
     createCoupon() {
       this.couponForm.data.is_enabled -= 0;
+      const apiRouter = this.couponApi === 'post' ? '/admin/coupon' : `/admin/coupon/${this.couponForm.data.id}`;
+      const couponObj = this.couponApi === 'post' ? {
+        ...this.couponForm,
+        due_date: this.getTimeStamp()
+      } : { ...this.couponForm };
+      console.log(couponObj);
+
+
       this.$axios
-        .post('/admin/coupon',
-          {
-            ...this.couponForm,
-            due_date: new Date().getTime()
-          })
-        .then(() => {
-          alert('新增成功')
+      [this.couponApi](apiRouter, couponObj)
+        .then((res) => {
+          console.log(res);
+          alert(this.couponApi === 'post' ? '新增成功' : '編輯成功')
           this.closeModal();
+          this.getCoupons();
         })
         .catch(() => {
           alert('新增失敗')
           this.closeModal();
         })
     },
+    editCoupon(event, icoupon) {
+      if (event.target.type === 'checkbox') return;
+      this.couponApi = 'put';
+      this.couponForm = { data: { ...icoupon } }
+      this.modal.show()
+    },
+    handleCheckboxChange(icoupon) {
+      const { title, is_enabled, percent, due_date, id, code } = icoupon;
+      this.$axios
+        .put(`/admin/coupon/${id}`, {
+          "data": { title, percent, due_date, code, is_enabled: is_enabled - 0 }
+        })
+        .then((res) => {
+          if (res.data.success) alert('已更新優惠券');
+        })
+        .catch((err) => console.log(err))
+    },
     openModal() {
+      this.couponApi = 'post';
+      this.couponForm = {
+        data: {
+          title: "",
+          is_enabled: 0,
+          percent: null,
+          due_date: 0,
+          code: "testCode"
+        }
+      };
       this.modal.show()
     },
     closeModal() {
@@ -148,8 +198,8 @@ export default {
     if (!hasToken) {
       this.$router.push('/')
     } else {
-      this.$axios.defaults.headers.common['Authorization'] = docCookies.getItem('token')
-      this.getCoupons()
+      this.$axios.defaults.headers.common['Authorization'] = docCookies.getItem('token');
+      this.getCoupons();
       this.modal = new bootstrap.Modal(this.$refs.couponModal);
     }
   }
